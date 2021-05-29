f61ad298eb86a4d3a42d7deddff2fd563c166e4c
Alejo Gaitán <thy.gaitan@gmail.com>
2021-05-29 11:37:03 -0300

    Find Adjacents

---
+++ Pathfinders - Match 3 (Unity)/Assets/_Scripts/Block/Node.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Node : MonoBehaviour
+{
+    [SerializeField] private BlockModel currentBlock;
+    [SerializeField] private Vector2 nodeID;
+    [SerializeField] private List<Node> neighbourNodes;
+    public Vector2 NodeID 
+    {
+        get { return nodeID; }
+        set { nodeID = value; }
+    }
+
+    public BlockModel CurrentBlock
+    {
+        get { return currentBlock; }
+        set { currentBlock = value; }
+    }
+
+    public void GetNeighbours(List<Node> nodesList)
+    {
+        int foundAmount = 0;
+
+        for (int x = (int)nodeID.x -1; x < (int)nodeID.x + 2; x++)
+        {
+            for (int y = (int)nodeID.y - 1; y < (int)nodeID.y + 2; y++)
+            {
+                if (x == nodeID.x && y == nodeID.y) continue; // Skip own cell.
+
+                if (x == nodeID.x || y == nodeID.y) // Matchs adjacency
+                {
+                    Debug.Log("Found Adjacent Node");
+
+                    Node neighbour = FindNeighbourID(x, y, nodesList);
+                    if (neighbour != null && foundAmount < 4)
+                    {
+                        neighbourNodes.Add(neighbour);
+                        foundAmount++;
+                    }
+                }
+            }
+        }
+    }
+
+    private Node FindNeighbourID(int currX, int currY, List<Node> nodesList)
+    {
+        for (int i = 0; i < nodesList.Count; i++)
+        {
+            Node currentNode = nodesList[i];
+            if (currentNode.NodeID == new Vector2(currX, currY))
+            {
+                return currentNode;
+            }
+        }
+
+        return null;
+    }
+
+    public void PaintVecinos()
+    {
+        foreach (Node node in neighbourNodes)
+        {
+            node.GetComponent<Image>().color = Color.red;
+        }
+    }
+}
--- Pathfinders - Match 3 (Unity)/Assets/_Scripts/Grid/GridGenerator.cs
+++ Pathfinders - Match 3 (Unity)/Assets/_Scripts/Grid/GridGenerator.cs
@@ -5,9 +5,14 @@ using UnityEngine.UI;
 
 public class GridGenerator : MonoBehaviour
 {
+    public List<Node> gridNodeArray;
+
     [Header("Components")]
     [SerializeField] private GridLayoutGroup gridLayoutGroup;
     [SerializeField] private RectTransform rectTransform;
+
+    [Header("Prefabs")]
+    [SerializeField] private GameObject nodePrefab;
     [SerializeField] private GameObject blockPrefab;
 
     [Header("Customize")]
@@ -17,16 +22,43 @@ public class GridGenerator : MonoBehaviour
     private void Start()
     {
         rectTransform.sizeDelta = new Vector2(gridSize.x * cellSize, gridSize.y * cellSize);
-        SpawnBlocks();
+        SpawnNodes();
     }
 
-    private void SpawnBlocks()
+    private void SpawnNodes()
     {
-        float totalBlockAmount = (gridSize.x * gridSize.y);
-
-        for (int i = 0; i < totalBlockAmount; i++)
+        //float totalBlockAmount = (gridSize.x * gridSize.y);
+        for (int x = 0; x < gridSize.x; x++)
         {
-            Instantiate(blockPrefab, gridLayoutGroup.transform);
+            for (int y = 0; y < gridSize.y; y++)
+            {
+                var node = Instantiate(nodePrefab, gridLayoutGroup.transform);
+                node.name = "Node: " + $"[{x},{y}]";
+
+                Node nodeComponent = node.GetComponent<Node>();
+                nodeComponent.NodeID = new Vector2(x,y);
+                gridNodeArray.Add(node.GetComponent<Node>());
+                nodeComponent.CurrentBlock = SpawnBlock(node.transform);
+
+            }
+        }
+
+        GetNodeNeighbours();
+    }
+
+    private BlockModel SpawnBlock(Transform nodeTransform)
+    {
+        var node = Instantiate(blockPrefab, nodeTransform);
+        return node.GetComponent<BlockModel>();
+    }
+
+    private void GetNodeNeighbours()
+    {
+        for (int i = 0; i < gridNodeArray.Count; i++)
+        {
+            Node currentNode = gridNodeArray[i];
+            currentNode.GetComponent<Image>().color = Color.yellow;
+            currentNode.GetNeighbours(gridNodeArray);
         }
     }
 }
